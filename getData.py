f8e6bf6860c5fe8cb6a282f9bb38a5f30d543845
Advitya Dua <duaadvitya@gmail.com>
2026-03-28 11:26:45 +0530

    Fixed

--- getData.py
+++ getData.py
@@ -499,13 +499,15 @@ prefs = {
     "download.directory_upgrade": True,
 }
 chrome_options.add_experimental_option("prefs", prefs)
-chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-software-rasterizer")
 chrome_options.add_argument("--disable-accelerated-2d-canvas")
 chrome_options.add_argument("--disable-logging")
 chrome_options.add_argument("--v=1")
-chrome_options.add_argument("--headless")  # Headless mode (optional for debugging)
+chrome_options.add_argument("--headless=new")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--window-size=1920,1080")
 
 
 logger.info('GeM Data Capture Service started.')
